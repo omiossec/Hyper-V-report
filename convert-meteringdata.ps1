2e32247d70e21ef488e53004eb0239180cb6ee4b
Olivier Miossec <oliviermiossec@hotmail.fr>
2016-05-08 14:55:36 +0200

    first commit for convert-meteringdata.ps1

---
+++ convert-meteringdata.ps1
@@ -0,0 +1,54 @@
+#requires -Version 4
+<#
+        .NOTES  
+
+        File Name  	: Hyper-v-report.ps1  
+        Version		: 0.5
+        Author     	: Olivier Miossec olivier@mediactive.fr
+        
+        Requires   	: PowerShell V4+, Hyper-v 2012 +
+
+        .LINK  
+
+        http://blog.omiossec.work
+
+        .LINK  
+
+        http://www.mediactive-network.net
+
+        .SYNOPSIS
+
+         Take a VMMeteringReportForVirtualMachine object and return an array of map
+
+        .DESCRIPTION
+
+         
+#>
+
+function Convert-VMMeteringData
+{
+    
+        param (
+            [Parameter(Mandatory = $true)]
+            $VmMeteringData
+        )
+    
+foreach ($VMReport in $VMReports) 
+{
+   
+   if ($VMReport.GetType() -eq [Microsoft.HyperV.PowerShell.VMMeteringReportForVirtualMachine])
+    {
+        write-host 'OK'
+        # We want the network traffic for all vmnetadapter, a vm can have up to 8 netadapter, I use measure-object with -sum to calcul the trafic from all the adapter
+        $Outbound = $VMReport.NetworkMeteredTrafficReport | Where-Object { $_.RemoteAddress -eq "0.0.0.0/0" -AND $_.Direction -eq "OutBound" } |  Measure-Object TotalTraffic -Sum 
+        $inbound = $VMReport.NetworkMeteredTrafficReport | Where-Object { $_.RemoteAddress -eq "0.0.0.0/0" -AND $_.Direction -eq "InBound" } |  Measure-Object TotalTraffic -Sum 
+    
+        write-host "Traffic sortant " $VMReport.AggregatedDiskDataRead   $VMReport.AggregatedDiskDataWritten $VMReport.ComputerName $VMReport.VMId $VMReport.vmname
+        $Report=@{"AvgCPU"=$VMReport.AvgCPU.ToString();"AvgRam"=$VMReport.AvgRAM.ToString();"TotalDisk"=$VMReport.TotalDisk.ToString();"AggregatedAverageLatency" = $VMReport.AggregatedAverageLatency.ToString(); "AggregatedAverageNormalizedIOPS" = $VMReport.AggregatedAverageNormalizedIOPS.ToString();  "AggregatedDiskDataRead" = $VMReport.AggregatedDiskDataRead.ToString();  "AggregatedDiskDataWritten" = $VMReport.AggregatedDiskDataWritten.ToString(); "OutboundTraffic" = $Outbound.Sum; "InboundTraffic" = $inbound.Sum  }
+    }
+    else {
+        throw "No VM Metering data"
+    }
+}
+    
+}
